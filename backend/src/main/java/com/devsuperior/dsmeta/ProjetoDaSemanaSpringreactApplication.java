df61e2eae54af394214e404cb1f4072cf5b4f053
William Cavalcante <cavalwc@gmail.com>
2022-09-07 23:49:49 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsmeta/ProjetoDaSemanaSpringreactApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDaSemanaSpringreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDaSemanaSpringreactApplication.class, args);
+	}
+
+}
